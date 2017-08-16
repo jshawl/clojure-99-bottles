5d3a96d282f9643d64e1aa30702f1fc230824ae6
Jesse Shawl <jesse@jshawl.com>
2017-08-16 15:51:33 -0400

    add environ dep

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
+                 [environ "1.0.0"]
                  [ring/ring-json "0.3.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clojure-99-bottles.handler/app}
