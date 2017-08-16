189882594a3f80add07a9947b8c41d3f76902ecf
Jesse Shawl <jesse@jshawl.com>
2017-08-16 18:21:21 -0400

    add jetty dep

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
                  [ring/ring-json "0.3.1"]]
   :plugins [[lein-ring "0.9.7"]]
